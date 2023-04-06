96a5d5f3780ecedf82baf92d7fdf67339432c445
swog <sanicsupersonic@gmail.com>
2023-04-06 14:13:07 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,4 +1,22 @@
 
-public class Person {
-	
+public abstract class Person {
+	private String name;
+	private String address;
+	//private Date dateOfBirth;
+	private String email;
+	//private SSN ssn;
+
+	public abstract String toString();
+
+	public void setName(String Name) {
+		name = Name;
+	}
+
+	public void setAddress(String Address) {
+		address = Address;
+	}
+
+	public void setEmail(String Email) {
+		email = Email;
+	}
 }
