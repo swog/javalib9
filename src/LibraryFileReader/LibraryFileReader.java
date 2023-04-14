8fc5fe6a0a7b814e1770df677828f20ceb14b386
jme14 <williamsltrey3@gmail.com>
2023-04-14 13:05:23 -0500

    Changed FileReader to LibraryFileReader to remove conflict with java.io.FileReader

---
+++ src/LibraryFileReader/LibraryFileReader.java
@@ -0,0 +1,30 @@
+package src.FileReader;
+
+import java.io.FileReader;
+
+import src.Collection.Collection;
+
+/*
+ * STATIC METHODS
+ * > Collection readAllFilesIntoCompleteCollection()
+ * >> searches all valid files and adds them to the collection object
+ *
+ * > Collection readFileIntoCollection(String fileName)
+ * >> searches for an exact file and puts everything from that file into a collection
+ * */
+
+public class FileReader {
+
+    public static Collection readAllFilesIntoCompleteCollection(){
+        return null;
+    }
+
+
+    public static Collection readFileIntoCollection(String fileName){
+
+        try {
+
+        }
+        return null;
+    }
+}
