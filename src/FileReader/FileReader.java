e1245bc9ac6d166ade0c42346f910b1d6919dd26
jme14 <williamsltrey3@gmail.com>
2023-04-14 10:46:54 -0500

    file structure editing: moved code to src and .csvs to LibraryContentFiles

---
+++ src/FileReader/FileReader.java
@@ -0,0 +1,17 @@
+package javalib9.FileReader;
+
+import Collection;
+
+/*
+ * STATIC METHODS
+ * > readFileIntoCollection(String fileName)
+ * >> use this to recieve a Collection object featuring the data items found in the filename provided by the caller
+ * >> throws an exception on file not found so be sure to wrap this in a try...catch block
+ * */
+
+public class FileReader {
+
+    public static Collection readFileIntoCollection{
+        return null;
+    }
+}
