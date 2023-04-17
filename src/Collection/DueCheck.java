7cd227d912f59ef2ca7e17017a7c0a53c6356a97
WeenieMelon <manuelsurkc@gmail.com>
2023-04-17 18:15:57 -0500

    Commented out test code

--- src/Collection/DueCheck.java
+++ src/Collection/DueCheck.java
@@ -1,9 +1,11 @@
 package src.Collection;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import src.Collection.*;
 import src.Content.*;
+import src.Identifier.ISBN;
 import src.Person.*;
 import src.LibraryFileReader.*;
 
@@ -77,11 +79,27 @@ public class DueCheck {
     }
     /*public static void main(String[] args)
     {
-        System.out.println("Enter Test");
-        String File = "LibraryContentFiles/BookList.csv";
-		Collection bookCollection = new Collection("TestCollection");
-        bookCollection = LibraryFileReader.readFileIntoCollection(File,"books");
-        DueCheck newCheck = new DueCheck();
-        newCheck.newCheckOverdues(bookCollection);
+        Date d1= new Date();
+        Date d2 = new Date(123,03,30);
+        long diffInMillies = Math.abs(d2.getTime() - d1.getTime()); //Gets the number of milliseconds since the content has been checked out
+        int diffInDays =  (int) (diffInMillies / (1000*60*60*24));
+        System.out.println(diffInDays);
+        ISBN ISBN = new ISBN("0123456789");
+        Book testBook1 = new Book("Book1",ISBN,"Checked Out",-1,d1);
+        Book testBook2 = new Book("Book2",ISBN,"Checked Out",321,d2);
+        Book testBook3 = new Book("Book3",ISBN,"Not Checked Out",-1,d2);
+        Content [] contentArray = new Content[3];
+        contentArray[0]=testBook1;
+        contentArray[1]=testBook2;
+        contentArray[2]=testBook3;
+        Student student1 = new Student("Johnny", "Mizzou", d2, "JohhnyEmail", null, 0,-1,321);
+        Professor prof1= new Professor("Toby", "Columbia", d2, "TobyEmail", null, 0, 321);
+        
+        people[0]=student1;
+        people[1]=prof1;
+        Person.setPeople(people);
+        Collection library = new Collection("TestLibrary", contentArray);
+        DueCheck dueCheck = new DueCheck();
+        dueCheck.newCheckOverdues(library);
     }*/
 }
