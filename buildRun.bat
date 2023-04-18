87cffaa8d19488461367b65292454f5fd3a25001
swog <sanicsupersonic@gmail.com>
2023-04-17 22:00:32 -0500

    Update buildRun.bat

--- buildRun.bat
+++ buildRun.bat
@@ -1,4 +1,4 @@
 @echo off
-javac src/Person/*.java src/Collection/*.java src/Content/*.java src/LibraryFileReader/*.java src/Identifier/*.java
-java src/LibraryFileReader/PersonFileReader
+javac src/Person/*.java src/Collection/*.java src/Content/*.java src/LibraryFileReader/*.java src/Identifier/*.java src/Main/*.java
+java src/Main/Main
 pause
