6464344c46e9e423d474185bf3f9f316429b5df4
swog <sanicsupersonic@gmail.com>
2023-05-08 13:02:40 -0500

    Update Professor.java

--- src/main/java/com/javalib9/app/Person/Professor.java
+++ src/main/java/com/javalib9/app/Person/Professor.java
@@ -21,6 +21,14 @@ public class Professor extends Member {
 		super(Name, Address, DoB, Email, Social, Id,BalanceDue);
 	}
 
+	public static Professor login(int Id) {
+		
+	}
+
+	public ArrayList<Student> getStudents() {
+		return students;
+	}
+
 	public String toString() {
 		return String.format(
 			"Student:\n\tName: %s\n\tAddress: %s\n\tDate of Birth: %s\n\tEmail: %s\n\tSSN: %s\n\tMember Id: %d\n", 
