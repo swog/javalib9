48a7ec803076845d738c480964fef84aacefa99d
WeenieMelon <manuelsurkc@gmail.com>
2023-05-10 16:50:28 -0500

    Update DueCheck.java
    DueCheck now returns an ArrayList<String>: Member email, Days Checked Out, Message for Dueness, amount fined. It also uses the file reader instead of requiring an input for the collection

--- src/main/java/com/javalib9/app/Collection/DueCheck.java
+++ src/main/java/com/javalib9/app/Collection/DueCheck.java
@@ -3,7 +3,7 @@ package com.javalib9.app.Collection;
 import java.util.ArrayList;
 import java.util.Date;
 
-import com.javalib9.app.Collection.*;
+//import com.javalib9.app.Collection.*;
 import com.javalib9.app.Content.*;
 import com.javalib9.app.Identifier.ISBN;
 import com.javalib9.app.Person.*;
@@ -18,9 +18,10 @@ public class DueCheck {
     {
 
     }
-    public ArrayList<String> newCheckOverdues(Collection library)
+    public ArrayList<String> newCheckOverdues()
     {
-        //Collection library = new Collection("CheckoutCollection");
+        Collection library = new Collection("CheckoutCollection");
+        //Collection library = LibraryFileReader.readAllContentFilesIntoCollection("CheckoutCollection");
         Collection CheckedOutCollection = library.getCheckedOutCollectionSubset("CheckedOutCollection");
         Content [] ContentArray = CheckedOutCollection.getContentArray();
         for ( int i = 0 ; i < ContentArray.length ; i++ ){
@@ -34,7 +35,7 @@ public class DueCheck {
                 ArrayList<String> output = new ArrayList<String>();
                 output.add(memberEmail);
                 output.add(Integer.toString(diffInDays));
-                output.add("Approaching Reaching Deadline");
+                output.add("Approaching Deadline");
                 output.add("No fine added");
                 return output;
             }
@@ -63,7 +64,6 @@ public class DueCheck {
             {
                 ContentArray[i].markLost();
                 String memberEmail= getEmailByContent(ContentArray[i]);
-                //String memberAddress = getAddressByContent(ContentArray[i]);
                 ArrayList<String> output = new ArrayList<String>();
                 output.add(memberEmail);
                 output.add(Integer.toString(diffInDays));
@@ -97,29 +97,4 @@ public class DueCheck {
         Member member = Member.getMember(memberID);
         member.IncBalance();
     }
-    /*public static void main(String[] args)
-    {
-        Date d1= new Date();
-        Date d2 = new Date(123,03,30);
-        long diffInMillies = Math.abs(d2.getTime() - d1.getTime()); //Gets the number of milliseconds since the content has been checked out
-        int diffInDays =  (int) (diffInMillies / (1000*60*60*24));
-        System.out.println(diffInDays);
-        ISBN ISBN = new ISBN("0123456789");
-        Book testBook1 = new Book("Book1",ISBN,"Checked Out",-1,d1);
-        Book testBook2 = new Book("Book2",ISBN,"Checked Out",321,d2);
-        Book testBook3 = new Book("Book3",ISBN,"Not Checked Out",-1,d2);
-        Content [] contentArray = new Content[3];
-        contentArray[0]=testBook1;
-        contentArray[1]=testBook2;
-        contentArray[2]=testBook3;
-        Student student1 = new Student("Johnny", "Mizzou", d2, "JohhnyEmail", null, 0,-1,321);
-        Professor prof1= new Professor("Toby", "Columbia", d2, "TobyEmail", null, 0, 321);
-        
-        people[0]=student1;
-        people[1]=prof1;
-        Person.setPeople(people);
-        Collection library = new Collection("TestLibrary", contentArray);
-        DueCheck dueCheck = new DueCheck();
-        dueCheck.newCheckOverdues(library);
-    }*/
 }
