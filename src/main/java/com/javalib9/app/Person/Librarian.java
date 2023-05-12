abe837cd94a70e2979cae9cff1a2a7447d98141e
AllanJones24 <108021106+AllanJones24@users.noreply.github.com>
2023-05-11 23:02:18 -0500

    Updated Librarian Class
    Added methods to add/remove items from collection.
    
    Added librarian view methods to buttons.

--- src/main/java/com/javalib9/app/Person/Librarian.java
+++ src/main/java/com/javalib9/app/Person/Librarian.java
@@ -1,4 +1,5 @@
 package com.javalib9.app.Person;
+
 import com.javalib9.app.Content.Content;
 import com.javalib9.app.Content.Book;
 import com.javalib9.app.Content.DVD;
@@ -8,7 +9,6 @@ import com.javalib9.app.Collection.Collection;
 import com.javalib9.app.LibraryFileReader.LibraryFileReader;
 import com.javalib9.app.Identifier.*;
 import java.util.Date;
-
 
 public class Librarian extends Employee {
 
@@ -34,43 +34,92 @@ public class Librarian extends Employee {
 	// Manage Collections
 
 	// function to add a book to the library
-	public static void addBook(Content book) {
+	public static void addBook(String title, ISBN identifier) {
 		String File = "LibraryContentFiles/BookList.csv";
 		Collection bookCollection = LibraryFileReader.readFileIntoCollection(File, "books");
+		Content book = new Book(title, identifier);
 		bookCollection.addItem(book);
 		LibraryFileReader.writeBookCollectionIntoFile(bookCollection, File);
 		return;
 	}
 
 	// function to add a DVD to the library
-	public static void addDVD(Content dvd) {
+
+	public static void addDVD(String title, ISBN identifier) {
 		String File = "LibraryContentFiles/DVDList.csv";
 		Collection dvdCollection = LibraryFileReader.readFileIntoCollection(File, "DVDs");
+		Content dvd = new DVD(title, identifier);
 		dvdCollection.addItem(dvd);
 		LibraryFileReader.writeDVDCollectionIntoFile(dvdCollection, File);
 		return;
 	}
 
 	// function to add a newspaper to the library
-	public static void addNewspaper(Content newspaper) {
+	public static void addNewspaper(String title, ISSN identifier) {
 		String File = "LibraryContentFiles/NewspaperList.csv";
 		Collection newspaperCollection = LibraryFileReader.readFileIntoCollection(File, "newspaper");
+		Content newspaper = new Newspaper(title, identifier);
 		newspaperCollection.addItem(newspaper);
 		LibraryFileReader.writeNewspaperCollectionIntoFile(newspaperCollection, File);
 		return;
 	}
 
 	// function to add a journal to the library
-	public static void addJournal(Content journal) {
+	public static void addJournal(String title, ISSN identifier) {
 		String File = "LibraryContentFiles/JournalList.csv";
 		Collection journalCollection = LibraryFileReader.readFileIntoCollection(File, "journal");
+		Content journal = new Journal(title, identifier);
 		journalCollection.addItem(journal);
 		LibraryFileReader.writeJournalCollectionIntoFile(journalCollection, File);
 		return;
 	}
 
+	// function to add any item using switch case
+	public static void addItem(String identifier, String ContentType, String title) {
+
+		ISBN isbn = null;
+		ISSN issn = null;
+
+		if (ContentType == "Book" || ContentType == "DVD") {
+			try {
+				isbn = new ISBN(identifier);
+			} catch (InvalidIdentifierException e) {
+				System.out.println("Invalid ISBN");
+				return;
+			}
+		} else if (ContentType == "Newspaper" || ContentType == "Journal") {
+			try {
+				issn = new ISSN(identifier);
+			} catch (InvalidIdentifierException e) {
+				System.out.println("Invalid ISSN");
+				return;
+			}
+		} else {
+			System.out.println("Invalid type");
+			return;
+		}
+		switch (ContentType) {
+			case "Book":
+				addBook(title, isbn);
+				break;
+			case "DVD":
+				addDVD(title, isbn);
+				break;
+			case "Newspaper":
+				addNewspaper(title, issn);
+				break;
+			case "Journal":
+				addJournal(title, issn);
+				break;
+			default:
+				System.out.println("Invalid type");
+				break;
+		}
+		return;
+	}
+
 	// function to remove a book from the library by identifier
-	public static void removeBookByIdentifier(Identifier identifier) {
+	public static void removeBookByIdentifier(ISBN identifier) {
 		String File = "LibraryContentFiles/BookList.csv";
 		Collection bookCollection = LibraryFileReader.readFileIntoCollection(File, "books");
 		bookCollection.removeItemByIdentifier(identifier);
@@ -79,7 +128,7 @@ public class Librarian extends Employee {
 	}
 
 	// function to remove a DVD from the library by identifier
-	public static void removeDVDByIdentifier(Identifier identifier) {
+	public static void removeDVDByIdentifier(ISBN identifier) {
 		String File = "LibraryContentFiles/DVDList.csv";
 		Collection dvdCollection = LibraryFileReader.readFileIntoCollection(File, "DVDs");
 		dvdCollection.removeItemByIdentifier(identifier);
@@ -88,7 +137,7 @@ public class Librarian extends Employee {
 	}
 
 	// function to remove a newspaper from the library by identifier
-	public static void removeNewspaperByIdentifier(Identifier identifier) {
+	public static void removeNewspaperByIdentifier(ISSN identifier) {
 		String File = "LibraryContentFiles/NewspaperList.csv";
 		Collection newspaperCollection = LibraryFileReader.readFileIntoCollection(File, "newspaper");
 		newspaperCollection.removeItemByIdentifier(identifier);
@@ -97,7 +146,7 @@ public class Librarian extends Employee {
 	}
 
 	// function to remove a journal from the library by identifier
-	public static void removeJournalByIdentifier(Identifier identifier) {
+	public static void removeJournalByIdentifier(ISSN identifier) {
 		String File = "LibraryContentFiles/JournalList.csv";
 		Collection journalCollection = LibraryFileReader.readFileIntoCollection(File, "journal");
 		journalCollection.removeItemByIdentifier(identifier);
@@ -105,38 +154,98 @@ public class Librarian extends Employee {
 		return;
 	}
 
+	// function to remove any item using switch case
+	public static void removeItem(String identifier) {
+
+		ISBN isbn = null;
+		ISSN issn = null;
+
+		try {
+			isbn = new ISBN(identifier);
+		} catch (InvalidIdentifierException e) {
+			System.out.println("Invalid ISBN");
+			return;
+		}
+
+		try {
+			issn = new ISSN(identifier);
+		} catch (InvalidIdentifierException e) {
+			System.out.println("Invalid ISSN");
+			return;
+		}
+
+		String File = "LibraryContentFiles/BookList.csv";
+		Collection bookCollection = LibraryFileReader.readFileIntoCollection(File, "books");
+		int i = bookCollection.searchItemByIdentifierForIndex(isbn);
+		if (i != -1) {
+			removeBookByIdentifier(isbn);
+			return;
+		}
+
+		File = "LibraryContentFiles/DVDList.csv";
+		Collection dvdCollection = LibraryFileReader.readFileIntoCollection(File, "DVDs");
+		i = dvdCollection.searchItemByIdentifierForIndex(isbn);
+		if (i != -1) {
+			removeDVDByIdentifier(isbn);
+			return;
+		}
+
+		File = "LibraryContentFiles/NewspaperList.csv";
+		Collection newspaperCollection = LibraryFileReader.readFileIntoCollection(File, "newspaper");
+		i = newspaperCollection.searchItemByIdentifierForIndex(issn);
+		if (i != -1) {
+			removeNewspaperByIdentifier(issn);
+			return;
+		}
+
+		File = "LibraryContentFiles/JournalList.csv";
+		Collection journalCollection = LibraryFileReader.readFileIntoCollection(File, "journal");
+		i = journalCollection.searchItemByIdentifierForIndex(issn);
+		if (i != -1) {
+			removeJournalByIdentifier(issn);
+			return;
+		}
+
+		System.out.println("Item not found");
+		return;
+	}
+
 	// Help customers with resources
 
 	// function to find a book in the library by identifier
-	public static Content findBookByIdentifier(Identifier identifier) {
-		String File = "LibraryContentFiles/BookList.csv";
-		Collection bookCollection = LibraryFileReader.readFileIntoCollection(File, "books");
-		return bookCollection.getItemByIdentifier(identifier);
-	}
+	// public static Content findBookByIdentifier(Identifier identifier) {
+	// String File = "LibraryContentFiles/BookList.csv";
+	// Collection bookCollection = LibraryFileReader.readFileIntoCollection(File,
+	// "books");
+	// return bookCollection.getItemByIdentifier(identifier);
+	// }
 
-	// function to find a DVD in the library by identifier
-	public static Content findDVDByIdentifier(Identifier identifier) {
-		String File = "LibraryContentFiles/DVDList.csv";
-		Collection dvdCollection = LibraryFileReader.readFileIntoCollection(File, "DVDs");
-		return dvdCollection.getItemByIdentifier(identifier);
-	}
+	// // function to find a DVD in the library by identifier
+	// public static Content findDVDByIdentifier(Identifier identifier) {
+	// String File = "LibraryContentFiles/DVDList.csv";
+	// Collection dvdCollection = LibraryFileReader.readFileIntoCollection(File,
+	// "DVDs");
+	// return dvdCollection.getItemByIdentifier(identifier);
+	// }
 
-	// function to find a newspaper in the library by identifier
-	public static Content findNewspaperByIdentifier(Identifier identifier) {
-		String File = "LibraryContentFiles/NewspaperList.csv";
-		Collection newspaperCollection = LibraryFileReader.readFileIntoCollection(File, "newspaper");
-		return newspaperCollection.getItemByIdentifier(identifier);
-	}
+	// // function to find a newspaper in the library by identifier
+	// public static Content findNewspaperByIdentifier(Identifier identifier) {
+	// String File = "LibraryContentFiles/NewspaperList.csv";
+	// Collection newspaperCollection =
+	// LibraryFileReader.readFileIntoCollection(File, "newspaper");
+	// return newspaperCollection.getItemByIdentifier(identifier);
+	// }
 
-	// function to find a journal in the library by identifier
-	public static Content findJournalByIdentifier(Identifier identifier) {
-		String File = "LibraryContentFiles/JournalList.csv";
-		Collection journalCollection = LibraryFileReader.readFileIntoCollection(File, "journal");
-		return journalCollection.getItemByIdentifier(identifier);
-	}
+	// // function to find a journal in the library by identifier
+	// public static Content findJournalByIdentifier(Identifier identifier) {
+	// String File = "LibraryContentFiles/JournalList.csv";
+	// Collection journalCollection = LibraryFileReader.readFileIntoCollection(File,
+	// "journal");
+	// return journalCollection.getItemByIdentifier(identifier);
+	// }
 
 	// Manage Memberships
 
-	// function to get member id
+	// function to add student to people.csv
 
 }
